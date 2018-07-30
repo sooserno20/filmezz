e2e4d6d542480406a713ac953153f9fcc1089fc7
Soos Erno <sooserno86@gmail.com>
2018-07-30 16:35:32 +0300

    Add search and pagination

--- core/views.py
+++ core/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.db.models import Q
 from django.views.generic import ListView
 from .models import Movie
 
 
 class MovieList(ListView):
     model = Movie
+    paginate_by = 10
     template_name = 'core/list.html'
+
+    def get_queryset(self):
+        try:
+            search = self.request.GET['search']
+        except KeyError:
+            object_list = self.model.objects.all()
+        else:
+            object_list = self.model.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
+        return object_list
