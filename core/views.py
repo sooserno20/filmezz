4020b84b1e773d405894afe1b900494e9641b356
erno.soos <erno.soos@spyhce.com>
2018-08-29 15:03:27 +0300

    TEMP COMMIT

--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
 from django.views.generic import DetailView, ListView
-from .models import Movie
+from .models import Movie, Category
+import unicodedata
+
+
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 
 class MovieList(ListView):
     model = Movie
     paginate_by = 12
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        category_options = list(Category.objects.all().values_list('name', flat=True))
+        data['category_options'] = sorted([strip_accents(s) for s in category_options])
+        return data
 
     def get_queryset(self):
         search_by = self.request.GET.get('search_by', 'title')
