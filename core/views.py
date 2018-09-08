cefa5d146ba3e592509f439142de09cd20a04bcd
Soos Erno <sooserno86@gmail.com>
2018-09-09 00:13:39 +0300

    Fixed search, tuned frontend

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import DetailView, ListView
+from django.db.models import Q
 from .models import Movie, Category, MovieLink
 import unicodedata
 import requests
@@ -9,6 +10,12 @@ def strip_accents(s):
     return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 
+def sort_accented_list(a_list):
+    a_dict = {strip_accents(s): s for s in a_list}
+    sorted_dict = sorted(a_dict.items(), key=lambda e: e[0])
+    return [e[1] for e in sorted_dict]
+
+
 class MovieList(ListView):
     model = Movie
     paginate_by = 12
@@ -16,7 +23,7 @@ class MovieList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         data = super().get_context_data(**kwargs)
         category_options = list(Category.objects.all().values_list('name', flat=True))
-        data['category_options'] = sorted([strip_accents(s) for s in category_options])
+        data['category_options'] = sort_accented_list(category_options)
         data['end_minus_five'] = data['paginator'].page_range.stop - 5
         data['last_page'] = data['paginator'].page_range.stop - 1
         # data['before_last_page'] = data['paginator'].page_range.stop - 2
@@ -27,23 +34,40 @@ class MovieList(ListView):
         return data
 
     def get_queryset(self):
-        search_by = self.request.GET.get('search_by', 'title')
+        search_by = self.request.GET.get('search_by', None)
         search = self.request.GET.get('search', None)
-        if not search:
-            return self.model.objects.all()
+        category = self.request.GET.get('category', None)
+        type = self.request.GET.get('type', None)
 
-        if search_by == 'title':
-            return self.model.objects.filter(title__icontains=search)
-        elif search_by == 'actors':
-            return self.model.objects.filter(actors__name__icontains=search)
-        elif search_by == 'directors':
-            return self.model.objects.filter(directors__name__icontains=search)
-        else:
-            return self.model.objects.all()
+        query_filter = []
+        if search:
+            if search_by == 'title':
+                query_filter.append(Q(title__icontains=search))
+            elif search_by == 'actors':
+                query_filter.append(Q(actors__name__icontains=search))
+            elif search_by == 'directors':
+                query_filter.append(Q(directors__name__icontains=search))
 
+        if category and category != 'All':
+            query_filter.append(Q(categories__name__icontains=category))
+
+        if type and type != 'All':
+            if type == 'Movie':
+                query_filter.append(Q(is_series=False))
+            elif type == 'Series':
+                query_filter.append(Q(is_series=True))
+
+        return self.model.objects.filter(*query_filter)
+        
 
 class MovieDetail(DetailView):
     model = Movie
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        data = super().get_context_data(**kwargs)
+        category_options = list(Category.objects.all().values_list('name', flat=True))
+        data['category_options'] = sort_accented_list(category_options)
+        return data
 
 
 def movie_link(request, movie_id, link_id):
